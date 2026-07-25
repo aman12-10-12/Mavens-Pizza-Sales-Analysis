1af59b1a93423be4bf4b999eecbab960bd2c1b8f
Aman Raj <amanraj@Mac.lan>
2026-07-25 10:22:53 +0530

    analysis: analyze peak hour orders and pizza quantity

---
+++ Queries & Insight/[05] Peak Hour Orders & Quantity.sql
@@ -0,0 +1,15 @@
+-- How many pizzas are we making during peak hour?
+
+SELECT DATEPART(HOUR, time) AS hours, 
+count(distinct(o.order_id)) as total_orders, 
+sum(od.quantity) as total_quantity 
+FROM pizza.dbo.orders o 
+JOIN pizza.dbo.order_details od 
+ON o.order_id = od.order_id
+GROUP BY DATEPART(HOUR, time) 
+ORDER BY total_orders DESC;
+
+At 12 p.m., the restaurant received 2,520 orders, which consisted of 6,776 pizzas in total.
+
+
+
